76345576829a012fdb0406fffa671d5da4cf5e6d
Umwari Marlene <m.umwari@alustudent.com>
2024-06-01 18:08:12 +0000

    Authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python3
+"""Authentication module for the API.
 """
-Auth class
-"""
-
-from flask import request
+import re
 from typing import List, TypeVar
+from flask import request
+import os
 
 
 class Auth:
-    """ Auth class
+    """Authentication class.
     """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth
-        """
-        if path is None:
-            return True
-        if excluded_paths is None or excluded_paths == []:
-            return True
-        path = path + '/' if path[-1] != '/' else path
-        excluded_paths = [excluded + '/' if excluded[-1] != '/' else excluded
-                          for excluded in excluded_paths]
 
-        if path not in excluded_paths:
-            return True
-        else:
-            return False
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
+        """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header
+        """Gets the authorization header field from the request.
         """
-        if request is None:
-            return None
-        if 'Authorization' not in request.headers:
-            return None
-        return request.headers['Authorization']
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user
+        """Gets the current user from the request.
         """
         return None
+
+    def session_cookie(self, request=None):
+        """Gets the session cookie from the request.
+        """
+        if request is not None:
+            return request.cookies.get(os.getenv('SESSION_NAME'))
--- Session_authentication/README.md
+++ Session_authentication/README.md
@@ -2,6 +2,7 @@
 
 Simple HTTP API for playing with `User` model.
 
+
 ## Files
 
 ### `models/`
@@ -15,20 +16,20 @@ Simple HTTP API for playing with `User` model.
 - `views/index.py`: basic endpoints of the API: `/status` and `/stats`
 - `views/users.py`: all users endpoints
 
+
 ## Setup
 
-``python
+```
 $ pip3 install -r requirements.txt
+```
 
-``
 
 ## Run
 
-``python
-
+```
 $ API_HOST=0.0.0.0 API_PORT=5000 python3 -m api.v1.app
+```
 
-``
 
 ## Routes
 
